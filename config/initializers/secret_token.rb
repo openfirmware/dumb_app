8df904d6cd7d281d238171b4e463d77e531fb837
James Badger <jamesbadger@gmail.com>
2012-01-12 13:35:42 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DumbApp::Application.config.secret_token = '7e3d2a650cf1f842cf2d163308cbace42e17484aed8792918c12a0a876ebf192aa5aebc926c9cadc08b4d2190d9d0bd7c0b496f504f3dd901322f7167d04afd7'
